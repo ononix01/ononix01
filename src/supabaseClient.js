63a2a2ec21cb49954f32364d37693b884e4cabeb
ko0306 <kouki0306@docomo.ne.jp>
2025-11-30 17:01:12 +0900

    initial commit

--- src/supabaseClient.js
+++ src/supabaseClient.js
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = 'https://csyjgivzvkypwhococfv.supabase.co';
-const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImNzeWpnaXZ6dmt5cHdob2NvY2Z2Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTEwNzA1MjIsImV4cCI6MjA2NjY0NjUyMn0.z4VP9e98Mg6_tipaV_TPgznb01iHSg_cXynKtj27HuU';
+const supabaseUrl = 'https://tqaeoyfsmrhqamtcsztd.supabase.co';
+const supabaseAnonKey = 'sb_publishable_Shmx_2S_309jzv7OqcjCYA_20Shwjwj';
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
