be344dbb2b0f44e070f9a9892296fd21167282a8
ko0306 <kouki0306@docomo.ne.jp>
2025-11-30 18:09:57 +0900

    変更内容

--- src/supabaseClient.js
+++ src/supabaseClient.js
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js';
 
 const supabaseUrl = 'https://tqaeoyfsmrhqamtcsztd.supabase.co';
-const supabaseAnonKey = 'sb_publishable_Shmx_2S_309jzv7OqcjCYA_20Shwjwj';
+const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InRxYWVveWZzbXJocWFtdGNzenRkIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjQyOTA2NzcsImV4cCI6MjA3OTg2NjY3N30.xr3Q2L7Od7a4cBouHkDN9t3mx-71afsCJ2r90Wb03XY';
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
